5213343a9a7d482540d36ac08f6c31fd21be9ffc
Milan <milance139@outlook.com>
2023-12-14 16:55:49 +0100

    Implemented Pagginator shared component

---
+++ BlazorEcommerce/Client/Shared/Pagginator.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorEcommerce.Client.Shared
+{
+    public partial class Pagginator
+    {
+        [Parameter]
+        public PagginationBaseModel Paggination { get; set; }
+        [Parameter]
+        public Func<int, Task> OnPageChange { get; set; }
+        public int CurrentPage => Paggination.CurrentPage;
+    }
+}
